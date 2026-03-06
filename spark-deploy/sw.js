14d04055d68b3b3d26a2d0fa7851bd403f328ba0
MoshpitKit <maddie20.hybridblock@gmail.com>
2026-03-06 20:49:02 +0800

    Add files via upload

--- spark-deploy/sw.js
+++ spark-deploy/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spark-v2';
+const CACHE_NAME = 'spark-v3';
 const URLS_TO_CACHE = ['./', './index.html'];
 
 self.addEventListener('install', event => {
@@ -19,6 +19,10 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request).then(response => {
+      const clone = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+      return response;
+    }).catch(() => caches.match(event.request))
   );
 });
